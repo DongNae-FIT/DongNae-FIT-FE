893b740402069787e3f62ad358f3eee7cdfa985c
JeongsuGil <lucy2468@naver.com>
2024-11-28 03:24:56 +0900

    20241128_03:24

--- src/pages/LoginPages/Login.jsx
+++ src/pages/LoginPages/Login.jsx
@@ -4,11 +4,6 @@ import { useTranslation } from "react-i18next";
 const Login = () => {
   const { t, i18n } = useTranslation();
 
-  const currentLang = i18n.language;
-
-  const kakaoLoginImgSrc =
-    currentLang === "ko" ? "/kakao_login_kr.png" : "/kakao_login_eng.png";
-
   const Rest_API_Key = "c4466ede44fd01c7de0788a9d99cf576";
   const redirect_uri = "http://localhost:5173/auth";
 
@@ -21,13 +16,15 @@ const Login = () => {
     <div className={styles["login"]}>
       <div className={styles["login__title"]}>{t("login.title")}</div>
       <div className={styles["login__img"]}></div>
-
-      <img
-        className={styles["login__kakao"]}
-        src={kakaoLoginImgSrc}
-        alt="Kakao Login"
-        onClick={handleLogin}
-      />
+      <div className={styles["kakao-login"]}>
+        <img
+          className={styles["kakao-login__symbol"]}
+          src={"/kakao_symbol.png"}
+          alt="Kakao Login"
+          onClick={handleLogin}
+        />
+        <div className={styles["kakao-login__text"]}>{t("login.kakao")}</div>
+      </div>
     </div>
   );
 };
